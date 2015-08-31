87f177de88ae9f3c6fcf58fde6fb9f406cd07322
TristanAgaesse <TristanAgaesse@users.noreply.github.com>
2015-08-31 18:25:10 +0200

    ComputeVoxelMatch : changed some variable names

--- Sources/PostProcessing/ComputeVoxelbyVoxelMatch.m
+++ Sources/PostProcessing/ComputeVoxelbyVoxelMatch.m
@@ -3,16 +3,16 @@ function voxelByVoxelError=ComputeVoxelbyVoxelMatch(image1,image2,labels)
     nLabel = length(labels);
     voxelByVoxelError = zeros(nLabel,1);
 
-    [expLabelEnds,expOrderLabels,expLabelIndices] = PoreNetworkImageBased.ParseLabeledImage(image1);
-    [simuLabelEnds,simuOrderLabels,simuLabelIndices] = PoreNetworkImageBased.ParseLabeledImage(image2);
+    [labelEnds1,orderLabels1,labelIndices1] = PoreNetworkImageBased.ParseLabeledImage(image1);
+    [labelEnds2,orderLabels2,labelIndices2] = PoreNetworkImageBased.ParseLabeledImage(image2);
     
     for i = 1:nLabel
         
         iLabel = labels(i);
-        expVoxels = PoreNetworkImageBased.GetVoxelsOfLabel(iLabel,expLabelEnds,expOrderLabels,expLabelIndices);
-        simuVoxels = PoreNetworkImageBased.GetVoxelsOfLabel(iLabel,simuLabelEnds,simuOrderLabels,simuLabelIndices);
+        voxels1 = PoreNetworkImageBased.GetVoxelsOfLabel(iLabel,labelEnds1,orderLabels1,labelIndices1);
+        voxels2 = PoreNetworkImageBased.GetVoxelsOfLabel(iLabel,labelEnds2,orderLabels2,labelIndices2);
         
-        voxelByVoxelError(i) = numel(intersect(expVoxels,simuVoxels))/numel(expVoxels);
+        voxelByVoxelError(i) = numel(intersect(voxels1,voxels2))/numel(voxels1);
         
     end
     
