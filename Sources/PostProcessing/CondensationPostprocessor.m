fc9b1ee9643eab65deed0c2d554695666ab5fb68
GreenToto <tristanagaesse@users.noreply.github.com>
2016-08-07 19:53:08 +0200

    Update Condensation postprocessor

--- Sources/PostProcessing/CondensationPostprocessor.m
+++ Sources/PostProcessing/CondensationPostprocessor.m
@@ -22,6 +22,8 @@ classdef CondensationPostprocessor
             postProcessor.CondensationClusters = condensationClusters;
         end
         
+        
+        %% Upscaled regions look up table
         function poreLookUpTable = BuildPoreLookUpTable(postProcessor,geometryRegions)
             %returns a look up table to know which pores are in each block, and which blocks contains a pore
             %Input : - postProcessor
@@ -33,7 +35,7 @@ classdef CondensationPostprocessor
             %   infos_Discretization.X = [xmin,xmax];
             %   infos_Discretization.Y = [ymin,ymean,ymax];
             %   infos_Discretization.Z = linspace(zmin,zmax,10);
-            %   [geometryRegions,geometryRegions3DIndex] = postProcessor.GetGeometryRegions_XYZDiscretized(infos_Discretization)
+            %   [geometryRegions,geometryRegions3DIndex] = postProcessor.BuildGeometryRegions_XYZDiscretized(infos_Discretization)
             %   region_x1y2z5 = geometryRegions{geometryRegions3DIndex(1,2,5)}
             %   poreLookUpTable = postProcessor.BuildPoreLookUpTable(geometryRegions)
             %   poresInRegion_x1y2z5 = find(ismember(poreLookUpTable,geometryRegions3DIndex(1,2,5)))
@@ -47,14 +49,48 @@ classdef CondensationPostprocessor
             for iRegion=1:nRegion
                 thisRegion = geometryRegions{iRegion};
                 
-                xfilter = and(poreCenters(:,1)>=thisRegion.Xmin,poreCenters(:,1)<=thisRegion.Xmax) 
-                yfilter = 
-                
+                xfilter = and(poreCenters(:,1)>=thisRegion.Xmin,poreCenters(:,1)<=thisRegion.Xmax) ;
+                yfilter = and(poreCenters(:,2)>=thisRegion.Ymin,poreCenters(:,2)<=thisRegion.Ymax) ;
+                zfilter = and(poreCenters(:,3)>=thisRegion.Ymin,poreCenters(:,3)<=thisRegion.Ymax) ;
                 
             end
             
         end
+                
+        function [geometryRegions,geometryRegions3DIndex] = BuildGeometryRegions3DDiscretized(postProcessor,infos_Discretization)
+            % Put
+            % Input : postProcessor,infos_Discretization
+            % Output : geometryRegions,geometryRegions3DIndex
+            % Example : 
+            %   infos_Discretization=struct
+            %   infos_Discretization.X = [xmin,xmax];
+            %   infos_Discretization.Y = [ymin,ymean,ymax];
+            %   infos_Discretization.Z = linspace(zmin,zmax,10);
+            %   [geometryRegions,geometryRegions3DIndex] = postProcessor.BuildGeometryRegions_XYZDiscretized(infos_Discretization)
+            %   region_x1y2z5 = geometryRegions{geometryRegions3DIndex(1,2,5)}
+            
+            
+        end
         
+        function [geometryRegions,geometryRegions2DIndex] = BuildGeometryRegions2DDiscretized(postProcessor,infos_Discretization)
+            % Put
+            % Input : postProcessor,infos_Discretization
+            % Output : geometryRegions,geometryRegions3DIndex
+            % Example : 
+            %   infos_Discretization=struct
+            %   infos_Discretization.X = [xmin,xmax];
+            %   infos_Discretization.Y = [ymin,ymean,ymax];
+            %   [geometryRegions,geometryRegions2DIndex] = postProcessor.BuildGeometryRegions2DDiscretized(infos_Discretization)
+            %   region_x1y2 = geometryRegions{geometryRegions2DIndex(1,2)}
+            
+           
+            infos_Discretization.Z=[-Inf,+Inf];
+            [geometryRegions,geometryRegions3DIndex] = GetGeometryRegions3DDiscretized(postProcessor,infos_Discretization);
+            
+            geometryRegions2DIndex = geometryRegions3DIndex(:,:,1);
+        end
+        
+        %% Cluster evolution tree
         function [tree,treeInfos] = BuildClusterEvolutionTree(postProcessor)
             %returns a tree of the clusters evolution during CondendationClusterGrowth
             %Input : - postProcessor
@@ -65,39 +101,23 @@ classdef CondensationPostprocessor
         end
         
         
-        function [geometryRegions,geometryRegions3DIndex] = GetGeometryRegions3DDiscretized(postProcessor,infos_Discretization)
-            % Put
-            % Input : postProcessor,infos_Discretization
-            % Output : geometryRegions,geometryRegions3DIndex
-            % Example : 
-            %   infos_Discretization=struct
-            %   infos_Discretization.X = [xmin,xmax];
-            %   infos_Discretization.Y = [ymin,ymean,ymax];
-            %   infos_Discretization.Z = linspace(zmin,zmax,10);
-            %   [geometryRegions,geometryRegions3DIndex] = postProcessor.GetGeometryRegions_XYZDiscretized(infos_Discretization)
-            %   region_x1y2z5 = geometryRegions{geometryRegions3DIndex(1,2,5)}
+        %% Getters on field evolution
+        
+        function field = GetFieldVaporConcentration(postProcessor,iStep)
             
+            
+            nPore = postProcessor.Network.GetNumberOfPores;
+            field = zeros(nPore,1);
+            
+            %CondensationInfos.
             
         end
         
-        function [geometryRegions,geometryRegions2DIndex] = GetGeometryRegions2DDiscretized(postProcessor,infos_Discretization)
-            % Put
-            % Input : postProcessor,infos_Discretization
-            % Output : geometryRegions,geometryRegions3DIndex
-            % Example : 
-            %   infos_Discretization=struct
-            %   infos_Discretization.X = [xmin,xmax];
-            %   infos_Discretization.Y = [ymin,ymean,ymax];
-            %   [geometryRegions,geometryRegions2DIndex] = postProcessor.GetGeometryRegions2DDiscretized(infos_Discretization)
-            %   region_x1y2 = geometryRegions{geometryRegions2DIndex(1,2)}
-            
-           
-            infos_Discretization.Z=[-Inf,+Inf];
-            [geometryRegions,geometryRegions3DIndex] = GetGeometryRegions3DDiscretized(postProcessor,infos_Discretization);
-            
-            geometryRegions2DIndex = geometryRegions3DIndex(:,:,1);
-        end
+        function field = GetFieldLiquidSaturation(postProcessor,iStep)
         
+            nPore = postProcessor.Network.GetNumberOfPores;
+            field = zeros(nPore,1);
+        end
         
         
     end
